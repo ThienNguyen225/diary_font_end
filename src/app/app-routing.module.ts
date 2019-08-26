799c9e635aa2861c90786d1fce124ea508700f25
Tuan250498 <vduy4245@gmail.com>
2019-08-26 08:35:56 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
 ];
 
